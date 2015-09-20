fce8b93016691e899244ecdfbb34c74bdb2e5ee2
Håkan Råberg <hraberg@yahoo.se>
2015-09-20 18:49:50 +0100

    Refactor

--- src/domaren/core.cljs
+++ src/domaren/core.cljs
@@ -11,91 +11,102 @@
        :private true}
   re-tag #"([^\s\.#]+)(?:#([^\s\.#]+))?(?:\.([^\s#]+))?")
 
-(declare render-component-fn!)
+(declare component->dom! hiccup->dom!)
 
 (defn node-attributes [dom-node]
   (let [attributes (.-attributes dom-node)]
     (areduce attributes idx acc []
              (conj acc (.-name (.item attributes idx))))))
 
+(defn hiccup? [x]
+  (and (vector? x) (keyword? (first x))))
+
+(defn component? [x]
+  (and (vector? x) (fn? (first x))))
+
+(defn html->dom! [dom-node hiccup]
+  (let [[tag & [attributes :as children]] hiccup
+        [attributes children] (if (map? attributes)
+                                [attributes (rest children)]
+                                [{} children])
+        [_ tag id class] (re-find re-tag (name tag))
+        classes (s/split class ".")
+        element (if (= (s/upper-case tag) (some-> dom-node .-tagName))
+                  dom-node
+                  (let [element (.createElement js/document tag)]
+                    (set! (.-__domaren element) #js {})
+                    element))
+        key-map (or (-> element .-__domaren .-keys) #js {})
+        new-key-map #js {}]
+    (when-not (= (-> element .-__domaren .-hiccup) hiccup)
+      (doseq [[k v] {"id" id "className" (s/join " " classes)}
+              :when (not= (aget element k) v)]
+        (aset element k (or v "")))
+      (doseq [[k v] attributes
+              :let [k (name k)]
+              :when (not= (.getAttribute element k) v)]
+        (.setAttribute element k (or v "")))
+      (doseq [k (remove (into #{"id" "className"} (map name (keys attributes)))
+                        (node-attributes element))]
+        (.removeAttribute element k))
+      (loop [[h & hs] children child (.-firstChild element)]
+        (cond
+          (seq? h)
+          (recur (concat h hs) child)
+
+          h
+          (let [key (some-> h meta :key str)
+                old-child (aget key-map key)
+                child (if (and child old-child
+                               (not= child old-child))
+                        (do
+                          (.replaceChild element old-child child)
+                          old-child)
+                        child)
+                new-child (hiccup->dom! child h)]
+            (when key
+              (aset new-key-map key new-child))
+            (cond
+              (and child (not= child new-child))
+              (.replaceChild element new-child child)
+
+              (not child)
+              (.appendChild element new-child))
+            (recur hs (.-nextSibling new-child)))
+
+          :else
+          (when-let [last-child (some-> child .-previousSibling)]
+            (while (.-nextSibling last-child)
+              (.remove (.-nextSibling last-child))))))
+      (set! (.-keys (.-__domaren element)) new-key-map)
+      (set! (.-hiccup (.-__domaren element)) hiccup))
+    element))
+
+(defn text->dom! [dom-node text]
+  (if (and dom-node (= (.-TEXT_NODE js/Node)
+                       (.-nodeType dom-node)))
+    (do
+      (when-not (= (.-textContent dom-node) text)
+        (set! (.-textContent dom-node) text))
+      dom-node)
+    (.createTextNode js/document text)))
+
 (defn hiccup->dom! [dom-node hiccup]
   (cond
-    (and (vector? hiccup)
-         (fn? (first hiccup)))
-    (apply render-component-fn! dom-node hiccup)
+    (component? hiccup)
+    (apply component->dom! dom-node hiccup)
 
-    (vector? hiccup)
-    (let [[tag & [attributes :as children]] hiccup
-          [attributes children] (if (map? attributes)
-                                  [attributes (rest children)]
-                                  [{} children])
-          [_ tag id class] (re-find re-tag (name tag))
-          classes (s/split class ".")
-          element (if (= (s/upper-case tag) (some-> dom-node .-tagName))
-                    dom-node
-                    (let [element (.createElement js/document tag)]
-                      (set! (.-__domaren element) #js {})
-                      element))
-          key-map (or (-> element .-__domaren .-keys) #js {})
-          new-key-map #js {}]
-      (when-not (= (-> element .-__domaren .-hiccup) hiccup)
-        (doseq [[k v] {"id" id "className" (s/join " " classes)}
-                :when (not= (aget element k) v)]
-          (aset element k (or v "")))
-        (doseq [[k v] attributes
-                :let [k (name k)]
-                :when (not= (.getAttribute element k) v)]
-          (.setAttribute element k (or v "")))
-        (doseq [k (remove (into #{"id" "className"} (map name (keys attributes)))
-                          (node-attributes element))]
-          (.removeAttribute element k))
-        (loop [[h & hs] children child (.-firstChild element)]
-          (cond
-            (seq? h)
-            (recur (concat h hs) child)
-
-            h
-            (let [key (some-> h meta :key str)
-                  old-child (aget key-map key)
-                  child (if (and child old-child
-                                 (not= child old-child))
-                          (do
-                            (.replaceChild element old-child child)
-                            old-child)
-                          child)
-                  new-child (hiccup->dom! child h)]
-              (when key
-                (aset new-key-map key new-child))
-              (cond
-                (and child (not= child new-child))
-                (.replaceChild element new-child child)
-
-                (not child)
-                (.appendChild element new-child))
-              (recur hs (.-nextSibling new-child)))
-
-            :else
-            (when-let [last-child (some-> child .-previousSibling)]
-              (while (.-nextSibling last-child)
-                (.remove (.-nextSibling last-child))))))
-        (set! (.-keys (.-__domaren element)) new-key-map)
-        (set! (.-hiccup (.-__domaren element)) hiccup))
-      element)
+    (hiccup? hiccup)
+    (html->dom! dom-node hiccup)
 
     :else
-    (if (and dom-node (= (.-TEXT_NODE js/Node)
-                         (.-nodeType dom-node)))
-      (do
-        (when-not (= (.-textContent dom-node) hiccup)
-          (set! (.-textContent dom-node) hiccup))
-        dom-node)
-      (.createTextNode js/document hiccup))))
+    (text->dom! dom-node (str hiccup))))
 
 (defn should-component-update? [dom-node state]
   (or (not= (some-> dom-node .-__domaren .-state) state)
       @force-rerender))
 
-(defn render-component-fn! [dom-node f & state]
+(defn component->dom! [dom-node f & state]
   (let [state (vec state)]
     (if (should-component-update? dom-node state)
       (time
@@ -114,7 +125,7 @@
    (fn tick []
      (try
        (let [current-node (.-firstChild dom-node)
-             new-node (render-component-fn! current-node (maybe-deref f) (maybe-deref state))]
+             new-node (component->dom! current-node (maybe-deref f) (maybe-deref state))]
          (when-not (= new-node current-node)
            (set! (.-innerHTML dom-node) "")
            (.appendChild dom-node new-node)))
