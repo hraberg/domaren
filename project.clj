9d0f6b36fb87ac5b69fa4c43ef32e1b92266f7ff
Håkan Råberg <hraberg@yahoo.se>
2015-10-06 22:27:38 +0100

    :pedantic? :abort + formatting.

--- project.clj
+++ project.clj
@@ -3,26 +3,23 @@
   :url "http://hraberg.github.io/domaren/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-
   :plugins [[lein-cljsbuild "1.1.0"]
-            [lein-figwheel "0.4.0"]]
-
+            [lein-figwheel "0.4.0" :exclusions [org.clojure/clojure
+                                                org.codehaus.plexus/plexus-utils
+                                                org.clojure/tools.reader]]]
   :profiles {:dev {:resource-paths ["examples/todomvc/resources"]
                    :dependencies [[com.cemerick/piggieback "0.2.1"
                                    :exclude [org.clojure/clojurescript]]
-                                  [org.clojure/tools.nrepl "0.2.10"]]
+                                  [org.clojure/tools.nrepl "0.2.11"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
-
+  :pedantic? :abort
   :source-paths ["src"]
-
   :clean-targets ^{:protect false} ["examples/todomvc/resources/public/js/compiled"
                                     "examples/vdom-benchmark-domaren/web/js/compiled"
                                     "target"]
-
   :cljsbuild {
     :builds [{:id "todomvc"
               :source-paths ["src" "examples/todomvc/src"]
@@ -41,7 +38,6 @@
                          :asset-path "js/compiled/out"
                          :output-to "examples/vdom-benchmark-domaren/web/js/compiled/vdom-benchmark-domaren.js"
                          :optimizations :simple}}]}
-
   :figwheel {:nrepl-port 7888
              :nrepl-middleware ["cider.nrepl/cider-middleware"
                                 "cemerick.piggieback/wrap-cljs-repl"]})
